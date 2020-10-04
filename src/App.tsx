8cee120cc7b3140b39a58c9aa34f9f51fbc45fdb
DiovanePacheti <frontendestudo@gmail.com>
2020-10-04 11:03:57 -0300

    estudando materia-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {ThemeProvider, createMuiTheme } from '@material-ui/core';
+import Home from './pages/Home';
+
+
 
 function App() {
+  const theme = createMuiTheme({
+    spacing:4,
+    palette:{
+      // type:'dark',
+
+      primary:{
+        main:'#f44336',
+      },
+
+      secondary:{
+        main:'#3f51b5',
+      },
+
+      background:{
+        default:'#232323',
+        // dark: '#181818',
+        paper:'#232323',
+      },
+    },
+  });
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>{/** O Theme Provader envolve a aplicação */}
+      <Home />
+    </ThemeProvider>
   );
 }
 
